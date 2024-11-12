eca824bd912b9d438cdbe9b6147cd2d54c58a09c
Hyun <cfu344@naver.com>
2024-11-12 12:05:29 +0900

    sky 상태

--- weather/app.js
+++ weather/app.js
@@ -9,7 +9,8 @@ async function run() {
         console.log('예측 평균 기온 (TMP):', weatherData.avgTemp);
         console.log('예측 최저 기온 (TMN):', weatherData.minTemp);
         console.log('예측 최고 기온 (TMX):', weatherData.maxTemp);
-        console.log('예측 하늘 상태 (SKY):', weatherData.sky);
+        //01 맑음 //02 구름 조금 //03 구름 많이 //04 흐림
+        console.log('예측 하늘 상태 (SKY):', weatherData.sky); 
         console.log('예측 강수 확률 (POP):', weatherData.pop);
     } catch (error) {
         console.error('오류:', error.message);
